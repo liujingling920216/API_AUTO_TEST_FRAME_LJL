5aa3305a8f38c9f6760bc936877942aa7a8ad6a3
sophia <1040616989@qq.com>
2020-07-21 23:26:14 +0800

    编写了common包下的emailutils.py工具类

--- samples/re_test.py
+++ samples/re_test.py
@@ -1,14 +1,17 @@
-import json
 import re
 
+import jsonpath
 
-acture_value = {"tags": [{"id": 2,"name": "星标组","count": 0},{"id": 133,"name": "广东","count": 0},{"id": 136,"name": "广西","count": 4},{"id": 137,"name": "广东1","count": 0},{"id": 138,"name": "石湾03","count": 0}]}
+acture_value = {"tags": [{"id": 2,"name": "星标组","count": 0},
+                         {"id": 133,"name": "广东","count": 0},
+                         {"id": 136,"name": "广西","count": 4},
+                         {"id": 137,"name": "广东1","count": 0},
+                         {"id": 138,"name": "石湾03","count": 0}]}
+acture_value=str(acture_value["tags"][2])
+print(acture_value)
 
-str(acture_value["tags"][2])
-print(type(acture_value["tags"][2]))
-
-expect_value = '{"id":(.+?),"name":"(.+?)","count":(.+?)}'
-value = re.findall(expect_value,acture_value["tags"][2])
+expect_value = '{"id":136,"name":"(.+?)","count":4}'
+value = re.findall(expect_value,acture_value)
 if value:
     print(True)
 else:
